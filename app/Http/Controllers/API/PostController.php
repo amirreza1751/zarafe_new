f73da4fdf7cb0ce736795f81a0295b9555059fec
AmirReza <dashti.amir2752@gmail.com>
2019-05-15 14:33:18 +0430

    added test api

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+        foreach ($posts as $post){
+            $post->images = json_decode($post->images);
+            foreach ($post->images as $image){
+
+            }
+        }
+        return $posts;
+    }
 }
