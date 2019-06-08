cab99c295f6a57e755c1f949c8d37149a78f450c
AmirReza <dashti.amir2752@gmail.com>
2019-06-09 01:22:10 +0430

    add return value in exception render() method2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,12 +48,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-//       return parent::render($request, $exception);
-         return response()->json(
-             [
-                     'status' => 401,
-                     'message' => 'Unauthenticated'
-             ], 401
-         );
+       return parent::render($request, $exception);
+//         return response()->json(
+//             [
+//                     'status' => 401,
+//                     'message' => 'Unauthenticated'
+//             ], 401
+//         );
     }
 }
